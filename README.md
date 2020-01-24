dd47c281af49e2f0fcbf650b1d27831d093d0703
Egor <work@tern.ml>
2020-01-24 16:27:59 +0700

    1

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# DB-py
+# JSON config parser
 Json-based configuration parser, similar to configparser
+[Download](https://github.com/TheAngryPython/DB-py/blob/master/db.py)
+
+## Install
+```
+pip install jsondbparser
+```
+
+## Use
+```python
+import db
+db = db.parser('file.txt', encoding = 'utf-8', debug = False)
+
+db.add_section('section')
+# adds section
+
+db.set('section', 'name', 'value')
+# changes or adds value
+
+db.get('section', 'name')
+# returns 'value'
+
+db.get_section('section')
+# returns json section
+
+db.js()
+# returns full json
+
+db.save(file = 'file1.txt')
+# save db to file
+
+```
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="jsondbparser-TheAngryPython", # Replace with your own username
+	version="0.0.1",
+	author="Ternowoi Egor",
+	author_email="annom2017@mail.ru",
+	description="Json-based configuration parser, similar to configparser",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/TheAngryPython/DB-py",
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
